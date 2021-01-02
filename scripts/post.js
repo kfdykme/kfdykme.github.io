6e885d968ed4b44e4120205efbd759dae9384f6c
kfdykme <wim.k.f@live.com>
2021-01-02 23:12:22 +0800

    Site updated: 2021-01-02 23:12:19

---
+++ scripts/post.js
@@ -0,0 +1,5 @@
+let title = 'Android AppCompatActivity'
+let tag = '- ' + 'Android\n'
+let date = new Date()
+date = date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+console.info('---\ntitle: ' + title + '\ndate: ' + date + '\ntag:\n' + tag + '---\n' + '# ' + title )
